892a16a0f55ab23eba17ec69ea816f16753ba32f
darksouls <145496839+stoiccaa@users.noreply.github.com>
2024-12-16 23:42:21 +0100

    Bucle Arreglado FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -21,8 +21,9 @@ public class FizzBuzz {
 				System.out.println("Buzz");
 			} else if (i % 3 == 0 && i % 5 == 0) {
 				System.out.println("FizzBuzz");
-			} else
+			} else {
 				System.out.println(i);
+			}
 	}
 
 }
