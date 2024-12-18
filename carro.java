36e49890ac29df583d2dedd4813ccc0b70b30258
darksouls <145496839+stoiccaa@users.noreply.github.com>
2024-12-18 21:27:55 +0100

    Ahora se pueden quitar productos del carro

--- carro.java
+++ carro.java
@@ -12,17 +12,40 @@ public class carro {
 
 		System.out.println("Inserte un producto (N/A para terminar): ");
 		producto = sc.nextLine();
-		
+
 		while (!producto.equals("N/A")) {
 
 			productos.add(producto);
-			
+
 			System.out.println("Inserte un producto (N/A para terminar): ");
 			producto = sc.nextLine();
 
-			System.out.println("Productos en el carro: " +productos);
+			System.out.println("Productos en el carro: " + productos);
 		}
 
-		sc.close();
-	}
+		String removeOption;
+        do {
+            System.out.println("¿Desea eliminar algún producto? (Sí/No): ");
+            removeOption = sc.nextLine();
+
+            if (removeOption.equalsIgnoreCase("Sí")) { 
+                System.out.println("Ingrese el nombre del producto a eliminar: ");
+                String productoARemover = sc.nextLine();
+
+                if (productos.contains(productoARemover)) { 
+                    productos.remove(productoARemover); 
+                    System.out.println("Producto eliminado.");
+                } else {
+                    System.out.println("El producto no está en el carro.");
+                }
+
+                System.out.println("Productos actuales en el carro: " + productos);
+            }
+
+        } while (removeOption.equalsIgnoreCase("Sí")); 
+
+        System.out.println("Gracias por usar el carrito. Productos finales: " + productos);
+
+        sc.close();
+    }
 }
